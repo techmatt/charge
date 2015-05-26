68f75ddaaaaa34bfef8aa272d6fec1183de25c77
Matt <techmatt@gmail.com>
2015-05-26 10:05:11 -0700

    Remove backup stuff.

--- backup/oldCode/Coordinate.cpp
+++
@@ -1,3 +0,0 @@
-#include "Main.h"
-
-const Coordinate Coordinate::Invalid(-999, -999);
--- backup/oldCode/Main.cpp
+++
@@ -1,7 +0,0 @@
-/*
-Main.cpp
-Written by Matthew Fisher
-
-Source file for precompiled header.
-*/
-#include "Main.h"
